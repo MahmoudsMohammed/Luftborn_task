9a96876cbeb61fd53f09f006851a49190447f80a
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2024-10-25 14:33:40 +0300

    finish best sales component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { productService } from './services/product.service';
+import { BestSalesComponent } from './components/best-sales/best-sales.component';
+import { ProductComponent } from './components/product/product.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BestSalesComponent, ProductComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Luftborn_task';
+  productService = inject(productService);
 }
---
+++ src/app/components/best-sales/best-sales.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-best-sales',
+  standalone: true,
+  imports: [],
+  templateUrl: './best-sales.component.html',
+  styleUrl: './best-sales.component.scss'
+})
+export class BestSalesComponent {
+
+}
