5de4591dd418f6ad56e717353bdeafe66898b7bd
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2024-10-26 10:25:04 +0300

    finish the task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,40 @@
-import { Component, inject, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  inject,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { productService } from './services/product.service';
 import { BestSalesComponent } from './components/best-sales/best-sales.component';
 import { ProductComponent } from './components/product/product.component';
-import { Observable } from 'rxjs';
+import { fromEvent, map, Observable, tap } from 'rxjs';
 import { Product } from './model/product.model';
 import { AsyncPipe } from '@angular/common';
+import { LoaderComponent } from './components/loader/loader.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BestSalesComponent, ProductComponent, AsyncPipe],
+  imports: [
+    RouterOutlet,
+    BestSalesComponent,
+    ProductComponent,
+    AsyncPipe,
+    LoaderComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
+  @ViewChild('nav') nav!: ElementRef;
+  render = inject(Renderer2);
   productService = inject(productService);
   products$!: Observable<Product[]>;
+  loading: boolean = true;
+  toggle = true;
   colors = [
     '#FF8B64',
     '#55C2E6',
@@ -29,8 +47,32 @@ export class AppComponent implements OnInit {
     '#4bcf82',
     '#7335D2',
   ];
+  screenResize$ = fromEvent(window, 'resize');
 
   ngOnInit(): void {
     this.products$ = this.productService.getProducts();
+    setTimeout(() => {
+      this.loading = false;
+    }, 2500);
+    this.screenResize$
+      .pipe(
+        map((e) => (e.target as Window).innerWidth),
+        tap((w) => {
+          if (w > 767) {
+            this.render.setStyle(this.nav.nativeElement, 'display', 'block');
+            this.toggle = false;
+          }
+        })
+      )
+      .subscribe();
+  }
+
+  onToggle() {
+    if (this.toggle) {
+      this.render.setStyle(this.nav.nativeElement, 'display', 'block');
+    } else {
+      this.render.setStyle(this.nav.nativeElement, 'display', 'none');
+    }
+    this.toggle = !this.toggle;
   }
 }
