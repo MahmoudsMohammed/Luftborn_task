1e666499bf99f7edf25d5afc093f2985cd4b9f11
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2024-10-26 06:40:08 +0300

    finish responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,34 @@ import { RouterOutlet } from '@angular/router';
 import { productService } from './services/product.service';
 import { BestSalesComponent } from './components/best-sales/best-sales.component';
 import { ProductComponent } from './components/product/product.component';
+import { Observable } from 'rxjs';
+import { Product } from './model/product.model';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BestSalesComponent, ProductComponent],
+  imports: [RouterOutlet, BestSalesComponent, ProductComponent, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   productService = inject(productService);
+  products$!: Observable<Product[]>;
+  colors = [
+    '#FF8B64',
+    '#55C2E6',
+    '#FF5E7D',
+    '#FF8B64',
+    '#55C2E6',
+    '#4bcf82',
+    '#7335D2',
+    '#F1C75B',
+    '#4bcf82',
+    '#7335D2',
+  ];
+
+  ngOnInit(): void {
+    this.products$ = this.productService.getProducts();
+  }
 }
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../../model/product.model';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './product.component.html',
-  styleUrl: './product.component.scss'
+  styleUrl: './product.component.scss',
 })
 export class ProductComponent {
-
+  @Input({ required: true }) product!: Product;
+  @Input({ required: true }) color!: string;
 }
